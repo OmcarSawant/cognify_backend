d34f52c45571a71032e20ee8a43d1d9708ecb49f
OmcarSawant <123458301+OmcarSawant@users.noreply.github.com>
2026-01-15 20:19:29 +0530

    Change 1

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
     const server = createServer(app);
 
     server.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running !!`);
     });
 
     process.on('unhandledRejection', (err) => {
@@ -26,4 +26,5 @@ const PORT = process.env.PORT || 5000;
     process.exit(1);
   }
 })();
+
 
