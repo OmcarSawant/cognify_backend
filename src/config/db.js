5283a0905aded2cc91c25236fcf8041b05b78201
OmcarSawant <123458301+OmcarSawant@users.noreply.github.com>
2026-01-15 20:20:02 +0530

    Refactor MongoDB connection log message

--- src/config/db.js
+++ src/config/db.js
@@ -13,11 +13,12 @@ const connectDB = async () => {
 
   try {
     const conn = await mongoose.connect(uri);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected !!`);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
+
 
 export default connectDB;
